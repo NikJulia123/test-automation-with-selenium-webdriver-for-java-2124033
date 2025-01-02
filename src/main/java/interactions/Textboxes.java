b29f0140c5ba7c64c9c601de40f793afc4191bd1
NikJulia123 <126539252+NikJulia123@users.noreply.github.com>
2025-01-02 15:30:54 -0800

    interact with elements
    added the code to interact with web elements

--- src/main/java/interactions/Textboxes.java
+++ src/main/java/interactions/Textboxes.java
@@ -10,8 +10,13 @@ public class Textboxes extends Main {
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
 
-    
+    // TO INTERACT WITH ELEMENTS
+    WebElement textbox = driver.findElement(By.name("id-name1"));
+    textbox.getText(); // to read the text in this text box
+    System.out.println(textbox.getText());
+    textbox.clear(); // to clear the text in the text box
+    textbox.sendKeys("Hello!"); // to write text into the text box
 
-    driver.quit();
+    //driver.quit();
   }
 }
